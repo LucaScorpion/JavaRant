c148d08056ecfc5e64cfa63517d67f7e9ed45656
Luca Scalzotto <lucascalzotto@gmail.com>
2016-09-12 13:49:43 +0200

    Move networking logic to separate method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.scorpiac.javarant.Comment;
 import com.scorpiac.javarant.DevRant;
 import com.scorpiac.javarant.Rant;
 import com.scorpiac.javarant.Sort;
@@ -5,6 +6,7 @@ import com.scorpiac.javarant.Sort;
 public class Main {
     public static void main(String[] args) {
         Rant[] rants = DevRant.getRants(Sort.ALGO, 1, 0);
+        Comment[] comments = rants[0].getComments();
         System.out.println("Done.");
     }
 }
--- src/main/java/com/scorpiac/javarant/DevRant.java
+++ src/main/java/com/scorpiac/javarant/DevRant.java
@@ -1,6 +1,7 @@
 package com.scorpiac.javarant;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -8,7 +9,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DevRant {
@@ -18,30 +18,31 @@ public class DevRant {
     public static final String API_URL = BASE_URL + "/api/devrant";
     public static final String RANTS_URL = API_URL + "/rants";
 
-    private static final String appId = "3";
+    private static final String APP_ID = "3";
 
     public static Rant[] getRants(Sort sort, int limit, int skip) {
+        // Rants url, app id, sort, skip, limit.
+        String url = String.format("%1$s?app=%2$s&sort=%3$s&skip=%4$d&limit=%5$d", RANTS_URL, APP_ID, sort.toString(), skip, limit);
+        JsonElement json = request(url);
+
+        return json == null ? new Rant[0] : rantArrayFromJson(json.getAsJsonObject());
+    }
+
+    private static JsonElement request(String url) {
         HttpURLConnection connection;
         InputStream inputStream;
 
-        // Rants url, app id, sort, skip, limit.
-        String urlString = String.format("%1$s?app=%2$s&sort=%3$s&skip=%4$d&limit=%5$d", RANTS_URL, appId, sort.toString(), skip, limit);
-
         try {
             // Create the URL and connection, get the input stream.
-            URL url = new URL(urlString);
-            connection = (HttpURLConnection) url.openConnection();
+            connection = (HttpURLConnection) new URL(url).openConnection();
             inputStream = connection.getInputStream();
-        } catch (MalformedURLException m) {
-            m.printStackTrace();
-            return new Rant[0];
         } catch (IOException i) {
             i.printStackTrace();
-            return new Rant[0];
+            return null;
         }
 
-        // Read the response into a JSON object.
-        JsonObject json = new JsonParser().parse(new InputStreamReader(inputStream)).getAsJsonObject();
+        // Parse the response.
+        JsonElement json = new JsonParser().parse(new InputStreamReader(inputStream));
 
         // Close the stream reader, disconnect.
         try {
@@ -51,7 +52,7 @@ public class DevRant {
         }
         connection.disconnect();
 
-        return rantArrayFromJson(json);
+        return json;
     }
 
     private static Rant[] rantArrayFromJson(JsonObject json) {
