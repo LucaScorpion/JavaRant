e80e950ba8e928b55f4275a482edbefaf4a15185
Luca Scalzotto <luca@scorpiac.com>
2017-09-16 11:45:05 +0200

    Make Vote a class instead of enum, use the Reason

--- src/main/java/com/scorpiac/javarant/Vote.java
+++ src/main/java/com/scorpiac/javarant/Vote.java
@@ -1,22 +1,56 @@
 package com.scorpiac.javarant;
 
-public enum Vote {
-    UP(1),
-    NONE(0),
-    DOWN(-1);
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
-    private final int number;
+import java.util.ArrayList;
+import java.util.List;
 
-    Vote(int number) {
-        this.number = number;
+public class Vote {
+    /**
+     * An upvote.
+     */
+    public static final Vote UP = new Vote("1");
+    /**
+     * A neutral vote.
+     */
+    public static final Vote NONE = new Vote("0");
+
+    private final String value;
+
+    private Vote(String value) {
+        this.value = value;
     }
 
     /**
-     * Get the number value of this vote.
+     * A downvote.
      *
-     * @return The number value.
+     * @param reason The reason for the downvote.
+     * @return A downvote.
      */
-    public int getNumber() {
-        return number;
+    public static Vote DOWN(Reason reason) {
+        return new DownVote(reason);
+    }
+
+    List<NameValuePair> getOptions() {
+        List<NameValuePair> options = new ArrayList<>();
+        options.add(new BasicNameValuePair("vote", value));
+        return options;
+    }
+
+    private static class DownVote extends Vote {
+        private final Reason reason;
+
+        private DownVote(Reason reason) {
+            super("-1");
+            this.reason = reason;
+        }
+
+        @Override
+        List<NameValuePair> getOptions() {
+            List<NameValuePair> options = super.getOptions();
+            options.add(new BasicNameValuePair("reason", reason.toString()));
+            return options;
+        }
     }
 }
